11f4acf0f3f77bfd9d5fe74097fba8bf7d0269ef
Charlie <charliedavidcrane13@gmail.com>
2025-11-17 23:39:07 +0100

    ui: Changed the styling a bit and added transakt to the projects list, need a good photo for it though.

--- src/app/pages/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -1,11 +1,9 @@
-import {Component, HostBinding} from '@angular/core';
-import {TechStackComponent} from '../../components/tech-stack/tech-stack.component';
+import {Component} from '@angular/core';
 import {ProjectListComponent} from '../../components/project-list/project-list.component';
 
 @Component({
   selector: 'app-projects-page',
     imports: [
-        TechStackComponent,
         ProjectListComponent
     ],
   templateUrl: './projects-page.component.html',
--- src/app/shared/projects.ts
+++ src/app/shared/projects.ts
@@ -3,6 +3,17 @@ import { Project} from './models/project.model';
 export const projects : Project[] = [
     {
         id: "1",
+        title: "Transakt microservices",
+        description: "Transakt is an event-driven microservices banking system, built in Spring Boot, and the Spring cloud projects, alongside Keycloak for Authentication with OAuth2.0 and Kafka + Spring Cloud Stream for asynchronous messaging between services without tight coupling.",
+        urls: [
+            {
+                name: "Backend repository",
+                url: "https://github.com/ChxrlieExe33/transakt-monorepo"
+            }
+        ]
+    },
+    {
+        id: "2",
         title: "Social-Konnect",
         description: "Social Konnect is a social media platform I developed using Spring Boot and Angular, it has full JWT authentication, sign up with email verification, forgot password functionality and more. The application can be used similarly to instagram or twitter, it features users, posts, likes, comments, follows, etc. I optimised functionalities with asynchronous event listeners and handled queries of entities with multiple relationships in efficient ways.",
         urls: [
@@ -18,17 +29,6 @@ export const projects : Project[] = [
         images: [
             "/social-ui.jpg",
             "/search-page-better.png"
-        ]
-    },
-    {
-        id: "2",
-        title: "My-Portfolio",
-        description: "This is my portfolio",
-        urls: [
-            {
-                name: "Frontend repository",
-                url: "https://github.com/ChxrlieExe33/charles-portfolio"
-            }
         ]
     }
 ]
