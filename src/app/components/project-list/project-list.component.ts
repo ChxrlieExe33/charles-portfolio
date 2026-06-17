88a2f071b19a136fe8e50d9feb14ad2dc52ecc91
Charles <charliedavidcrane13@gmail.com>
2026-06-17 16:55:40 +0200

    other: Improved overall UI of portfolio.

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
-import {projects} from '../../shared/projects';
-import {ProjectComponent} from '../project/project.component';
+import { Component, signal } from '@angular/core';
+import { projects } from '../../shared/projects';
+import { ProjectComponent } from '../project/project.component';
+import { Project } from '../../shared/models/project.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-project-list',
     imports: [
-        ProjectComponent
+        ProjectComponent,
+        NgClass
     ],
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.css'
 })
 export class ProjectListComponent {
-
     protected readonly projects = projects;
+    activeProject = signal<Project | null>(projects.length > 0 ? projects[0] : null);
+
+    setActiveProject(project: Project) {
+      this.activeProject.set(project);
+    }
 }
